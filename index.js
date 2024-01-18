5f659f4b612084d4d7871648046cffc7d9c875ca
Akash Kadlag <akashkadlag14@gmail.com>
2024-01-18 18:32:05 +0530

    test: add integration tests for genres API endpoint

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from "express";
 
-import logger from "./logs/logger.js";
 import routes from "./startup/routes.js";
 import db from "./startup/db.js";
 import { handleUncaughtExceptions, handleUnhandledRejections } from "./middleware/handleErrors.js";
@@ -14,5 +13,5 @@ const app = express();
 routes(app);
 db();
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => logger.info(`Listening on Port ${port}...`));
+export default app;
+
--- startup/db.js
+++ startup/db.js
@@ -2,11 +2,15 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import logger from "../logs/logger.js";
 
+// Load environment variables based on NODE_ENV
+const environment = process.env.NODE_ENV || "development";
+const url = (environment === "test") ? process.env.TEST_DB_URL : process.env.DB_URL;
+
 const db = () => {
 	mongoose
-		.connect(process.env.DB_URL)
+		.connect(url)
 		.then(() => logger.info(`Connected to MongoDB...`))
-		.catch((ex) => console.error(ex));
+		.catch((ex) => logger.error(ex));
 };
 
 export default db;
