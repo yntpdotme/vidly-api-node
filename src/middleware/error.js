a12020d04ece345628956bd853cf0c2af6a13beb
Akash Kadlag <akashkadlag14@gmail.com>
2024-04-26 09:56:13 +0530

    refactor: enhance rate limiting mechanism for better performance and accuracy

--- src/middleware/error.js
+++ src/middleware/error.js
@@ -3,5 +3,5 @@ import logger from '../logger/logger.js';
 // eslint-disable-next-line no-unused-vars
 export default (err, req, res, next) => {
   logger.error(err.message, {err});
-  res.status(500).send('Something failed');
+  res.status(500).json(err?.message || 'Something failed');
 };
--- src/startup/prod.js
+++ src/startup/prod.js
@@ -14,19 +14,18 @@ const corsOptions = {
 };
 
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 300, // Limit each IP to 300 requests per `window` (here, per 15 minutes)
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 100, // Limit each IP to 100 requests per `window` (here, per minute)
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   keyGenerator: (req, res) => {
     return req.clientIp; // IP address from requestIp.mw(), as opposed to req.ip
   },
   handler: (_, __, ___, options) => {
-    throw new ApiError(
-      options.statusCode || 500,
+    throw new Error(
       `There are too many requests. You are only allowed ${
         options.max
-      } requests per ${options.windowMs / 60000} minutes`,
+      } requests per ${options.windowMs / 60000} minutes`
     );
   },
 });
