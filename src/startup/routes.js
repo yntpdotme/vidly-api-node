5558462f52dfef497218a4babaf3804206396eca
Akash Kadlag <akashkadlag14@gmail.com>
2024-11-26 21:17:42 +0530

    feat: add root route

--- src/startup/routes.js
+++ src/startup/routes.js
@@ -1,17 +1,25 @@
-import express from 'express';
 import cookieParser from 'cookie-parser';
+import express from 'express';
 
-import genresRoutes from '../routes/genreRoutes.js';
+import error from '../middleware/error.js';
 import customersRoutes from '../routes/customerRoutes.js';
+import employeeRoutes from '../routes/employeeRoutes.js';
+import genresRoutes from '../routes/genreRoutes.js';
 import moviesRoutes from '../routes/movieRoutes.js';
 import rentalRoutes from '../routes/rentalRoutes.js';
-import employeeRoutes from '../routes/employeeRoutes.js';
 import returnRoutes from '../routes/returnRoutes.js';
-import error from '../middleware/error.js';
 
 export default app => {
   app.use(express.json({limit: '16kb'}));
   app.use(cookieParser());
+
+  app.use('/', (req, res) => {
+    res.send(
+      `Welcome to Vidly Server.
+      <a href="https://documenter.getpostman.com/view/31850881/2sA3Bj8Zfc" target='_blank'>Explore API Docs</a>`,
+    );
+  });
+
   app.use('/api/genres', genresRoutes);
   app.use('/api/customers', customersRoutes);
   app.use('/api/movies', moviesRoutes);
